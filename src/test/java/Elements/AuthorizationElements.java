4ee925e5495c6280b2246d6b3ede1b9459fec8d6
kose <kostyasedikov@gmail.com>
2024-02-18 20:33:19 +0300

    push

--- src/test/java/Elements/AuthorizationElements.java
+++ src/test/java/Elements/AuthorizationElements.java
@@ -1,4 +1,12 @@
 package Elements;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 public class AuthorizationElements {
+    public static final SelenideElement     INPUT_USER_NAME_FIELD = $x("");
+    public static final SelenideElement INPUT_USER_PASSWORD_FIELD = $x("");
+    public static final SelenideElement        SUBMIT_DATA_BUTTON = $x("");
 }
--- src/test/java/TestSteps/AuthorizationSteps.java
+++ src/test/java/TestSteps/AuthorizationSteps.java
@@ -3,14 +3,31 @@ package TestSteps;
 import Elements.AuthorizationElements;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.Keys;
+
+import javax.sound.sampled.Control;
+
+import java.util.logging.Logger;
+
+import static java.lang.Character.CONTROL;
+import static java.lang.Character.isISOControl;
 
 public class AuthorizationSteps extends AuthorizationElements {
 
+    private String userName = "123";
+    private String userPass = "123";
     @Step
     @DisplayName("Authorize on stage: filling username")
     public AuthorizationSteps fillUsername()
     {
         // fill username
+        System.out.println("In user name input: ");
+        INPUT_USER_NAME_FIELD.click();
+        INPUT_USER_NAME_FIELD.sendKeys(Keys.CONTROL + "a");
+        INPUT_USER_NAME_FIELD.sendKeys(Keys.BACK_SPACE);
+        System.out.println("All deleted from field");
+        INPUT_USER_NAME_FIELD.setValue(userName);
+        System.out.println("Set value: "+userName);
         return this;
     }
 
@@ -19,6 +36,13 @@ public class AuthorizationSteps extends AuthorizationElements {
     public AuthorizationSteps fillPass()
     {
         // fill username
+        System.out.println("In password input: ");
+        INPUT_USER_PASSWORD_FIELD.click();
+        INPUT_USER_PASSWORD_FIELD.sendKeys(Keys.CONTROL + "a");
+        INPUT_USER_PASSWORD_FIELD.sendKeys(Keys.BACK_SPACE);
+        System.out.println("All deleted from the field");
+        INPUT_USER_PASSWORD_FIELD.setValue(userPass);
+        System.out.println("Set value: "+userPass);
         return this;
     }
     @Step
@@ -26,6 +50,8 @@ public class AuthorizationSteps extends AuthorizationElements {
     public AuthorizationSteps submitData()
     {
         // fill username
+        SUBMIT_DATA_BUTTON.click();
+        System.out.println("Data submitted!");
         return this;
     }
 }
